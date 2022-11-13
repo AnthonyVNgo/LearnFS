06d1f555a1c819032efc06cae2acb44b72de2028
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-13 14:57:53 -0800

    feat: add random letter function

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,17 @@
 
 <div>
   <p>userInput: {{ userInput }}</p>
-<input v-model="userInput" placeholder="edit me" />
+<input v-model="userInput" placeholder="edit me" maxlength="26"/>
   
-  <p v-if="randomWord === ''">ClickBbutton</p>
+  <p v-if="randomWord === ''">Click the button to...</p>
   <p v-else>
     {{ randomWord }}
   </p>
   <button @click="getRandomWord">
-    ASLerate
+    get word
+  </button>
+  <button @click="getRandomLetter">
+    get random letter
   </button>
 </div>  
   <!-- <RouterView /> -->
@@ -27,15 +30,23 @@ import { ref } from 'vue'
 
 const userInput = ref('')
 const randomWord = ref('')
+const randomLetter = ref('')
 
 
 function getRandomWord() {
-  fetch('https://random-words-api.vercel.app/word')
+  fetch('https://random-word-api.herokuapp.com/word?length=4')
   .then(res => res.json())
-  .then(data => randomWord.value = data[0].word.toLowerCase())
+  .then(data => randomWord.value = data[0])
   .catch(err => console.log(err))
 }
 
+function getRandomLetter() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  randomLetter.value = alphabet[Math.floor(Math.random() * alphabet.length)]
+  console.log(randomLetter.value)
+}
+
+// generate random letter from the alphabet 
 // call getRandomWord when app mounts 
 // remove v-if condition 
 // include two way binding between input and randomWord 
