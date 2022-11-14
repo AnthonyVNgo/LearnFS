51a4562757599e9135cab3de0de9827380a8fb17
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-13 22:08:25 -0800

    feat: add input and new letter button

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -2,20 +2,21 @@
 <div class="container">
   <div class="row">
     <div class="col">
-      <h1>Random Letter</h1>
-
       <div class="card ratio ratio-1x1">
         <div class="card-body" style="display: flex; justify-content: center; align-items: center;">
           <span style="font-size: 100px">{{randomLetter}}</span>
         </div>
       </div>
 
-      <button type="button" class="btn btn-dark" @click="getRandomLetter">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-          <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-        </svg>
-      </button>
+      <div>
+        <input type="email" class="form-control my-3" id="exampleFormControlInput1" placeholder="Type here" @keypress="getKey">
+        <button type="button" class="btn btn-dark" @click="getRandomLetter">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </div>
@@ -35,9 +36,14 @@ import { ref,
   onErrorCaptured } from 'vue';
 const randomLetter = ref('')
 
+function getKey() {
+  console.log('bang')
+}
+
 function getRandomLetter() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
   randomLetter.value = alphabet[Math.floor(Math.random() * alphabet.length)]
+  console.log(randomLetter.value)
 }
 
   onBeforeMount(() => {
