14e7e90d0b3b92f5c125ff88bc7504a542610b5a
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-13 21:04:26 -0800

    feat: fix onBeforeMount method

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -1,19 +1,63 @@
 <template>
   <h1>Random Letter</h1>
-  <p v-if="randomLetter === ''">Press the button</p>
-  <p v-else>{{randomLetter}}</p>
+  <!-- <p v-if="randomLetter === ''">Press the button</p> -->
+  <!-- <p v-else>{{randomLetter}}</p> -->
+  <p>{{randomLetter}}</p>
 
-  <button @click="getRandomLetter">
+  <!-- <button @click="getRandomLetter">
     get random letter
-  </button>
+  </button> -->
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref,   
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured } from 'vue';
 const randomLetter = ref('')
 
 function getRandomLetter() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
   randomLetter.value = alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+// onMounted(getRandomLetter())
+
+    onBeforeMount(() => {
+      console.log("Before Mount!");
+      getRandomLetter()
+    });
+    onMounted(() => {
+      console.log("Mounted!");
+    });
+    onBeforeUpdate(() => {
+      console.log("Before Update!");
+    });
+    onUpdated(() => {
+      console.log("Updated!");
+    });
+    onBeforeUnmount(() => {
+      console.log("Before Unmount!");
+    });
+    onUnmounted(() => {
+      console.log("Unmounted!");
+    });
+    onActivated(() => {
+      console.log("Activated!");
+    });
+    onDeactivated(() => {
+      console.log("Deactivated!");
+    });
+    onErrorCaptured(() => {
+      console.log("Error Captured!");
+    });
 </script>
+
+<style scoped>
+</style>
