dc6322aff50aff9048ac067228eaa565c2366d34
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-14 15:59:44 -0800

    feat: add $ref

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -8,9 +8,10 @@
         </div>
       </div>
 
-      <div>
-        <input type="email" class="form-control my-3" id="exampleFormControlInput1" placeholder="Type here" v-model="inputLetter">
+      <div tabindex="0" @keypress="consolelogBang" style="background-color: red;">
+        <input type="text" class="form-control my-3" id="exampleFormControlInput1" placeholder="Type here" v-model="inputLetter">
         <button type="button" class="btn btn-dark" @click="getRandomLetter">
+        <button type="button" class="btn btn-primary" @click="getRandomLetter">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
             <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
@@ -52,6 +53,14 @@ function getRandomLetter() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
   randomLetter.value = alphabet[Math.floor(Math.random() * alphabet.length)]
   // console.log(randomLetter.value)
+}
+
+function clearInput() {
+  
+}
+
+function consolelogBang() {
+  console.log(Event)
 }
 
 onBeforeMount(() => {
