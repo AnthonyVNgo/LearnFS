2af322cf9c68bdc2f235faa8a8aa2460b45f460b
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-15 00:21:10 -0800

    feat: cleanup RLview layout

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container" @keydown="handleKeyDown">
-  <div class="row">
+  <div class="row my-5">
     <div class="col">
       <div class="card ratio ratio-1x1">
         <div class="card-body" style="display: flex; justify-content: center; align-items: center;">
@@ -8,61 +8,67 @@
         </div>
       </div>
 
-      <div class="row justify-content-center">
-        <div class="col-auto">
-          <input type="text" class="form-control form-control-lg" id="" v-model="inputLetter" maxlength="1">
-        </div>
-        <div class="col-auto">
-          <button type="button" class="btn btn-dark" @click="getRandomLetter">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-            </svg>
-          </button>
-        </div>
-      </div>
-      
-      <div class="row">
+      <!-- Instructions  -->
+      <div class="row my-5">
         <div class="col">
-          <h3>Match the fingerspelling card with the correct letter</h3>
+          <h5>Match the fingerspelling card with the correct letter below</h5>
         </div>
       </div>
 
-<!-- Toast btn  -->
-<button type="button" class="btn btn-primary" id="toastbtn" @click="handleToastTrigger">Show Toast</button>
-
-<!-- Toast component  -->
-<div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="1000">
-  <div class="d-flex">
-    <div class="toast-body">
-      Hello, world! This is a toast message.
-    </div>
-    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-  </div>
-</div>
-
-
-<!-- Info Modal Trigger Button -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
-  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-</svg>
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Fingerspelling Diagram</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="row justify-content-center my-5">
+        <div class="col">
+          <div class="row justify-content-center mb-3">
+            <div class="col-auto">
+              <input type="text" class="form-control form-control-lg" id="" v-model="inputLetter" maxlength="1">
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-auto">
+              <!-- Get Random Letter Button  -->
+              <button type="button" class="btn btn-dark" @click="getRandomLetter">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                  <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                </svg>
+              </button>
+            </div>
+            <div class="col-auto">
+              <!-- Info Modal Trigger Button -->
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
+              </button>  
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        <img src="https://i.imgur.com/zqqSG9H.png" alt="" style="width: 100%;">
+
+      <!-- Toast  -->
+      <div class="toast-container position-fixed bottom-0 p-3">
+        <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+          <div class="d-flex">
+            <div class="toast-body">
+              Good job
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Fingerspelling Diagram</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <img src="https://i.imgur.com/zqqSG9H.png" alt="" style="width: 100%;">
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
@@ -110,6 +116,7 @@ function handleKeyDown(event) {
 watch(inputLetter, (newValue, oldValue) => {
   if (newValue === randomLetter) {
     getRandomLetter()
+    handleToastTrigger()
     inputLetter.value = ''
     // need to somehow clear the input value
   }
