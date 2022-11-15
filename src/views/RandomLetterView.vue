320af69a675b83866c12ab279682d507901c364a
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-14 23:20:38 -0800

    feat: add info button

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -28,6 +28,29 @@
         </div>
       </div>
 
+
+      <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
+  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Fingerspelling Diagram</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img src="https://i.imgur.com/zqqSG9H.png" alt="" style="width: 100%;">
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   </div>
 </div>
@@ -55,6 +78,7 @@ function getRandomLetter() {
 }
 
 let inputLetter = ref('')
+let input = ref('')
 
 function handleKeyDown(event) {
   if (event.which >= 65 && event.which <= 90) {
@@ -66,9 +90,9 @@ watch(inputLetter, (newValue, oldValue) => {
   if (newValue === randomLetter) {
     getRandomLetter()
     inputLetter.value = ''
+    // need to somehow clear the input value
   }
 })
-
 
 onBeforeMount(() => {
   getRandomLetter()
