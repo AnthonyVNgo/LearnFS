d071d7a10088885f5bedb62cdd341252848116c8
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-14 22:58:27 -0800

    feat: add instructions

--- src/views/RandomLetterView.vue
+++ src/views/RandomLetterView.vue
@@ -8,17 +8,26 @@
         </div>
       </div>
 
-      <div>
-        <!-- <input type="text" class="form-control my-3" id="exampleFormControlInput1" placeholder="Type here" v-model="inputLetter"> -->
-        <!-- <button type="button" class="btn btn-dark" @click="getRandomLetter"> -->
-        <span v-if="inputLetter !== ''">{{inputLetter}}</span>
-        <button type="button" class="btn btn-primary" @click="getRandomLetter">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
-            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
-          </svg>
-        </button>
+      <div class="row justify-content-center">
+        <div class="col-auto">
+          <input type="text" class="form-control form-control-lg" id="" v-model="inputLetter" maxlength="1">
+        </div>
+        <div class="col-auto">
+          <button type="button" class="btn btn-dark" @click="getRandomLetter">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+            </svg>
+          </button>
+        </div>
       </div>
+      
+      <div class="row">
+        <div class="col">
+          <h3>Match the fingerspelling card with the correct letter</h3>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
@@ -38,16 +47,6 @@ import { ref,
   onErrorCaptured,
   watch } from 'vue';
 
-
-
-// watch(inputLetter, (newLetter, oldLetter) => {
-//   console.log('new letter:', newLetter)
-//   console.log('old letter:', oldLetter)
-//   if (inputLetter === 'abc') {
-//     inputLetter = ''
-//   }
-// })
-
 let randomLetter = $ref('')
 
 function getRandomLetter() {
@@ -59,33 +58,25 @@ let inputLetter = ref('')
 
 function handleKeyDown(event) {
   if (event.which >= 65 && event.which <= 90) {
-    // console.log(event.key);
     inputLetter.value = event.key
   }
 }
 
 watch(inputLetter, (newValue, oldValue) => {
-  // console.log(newValue)
   if (newValue === randomLetter) {
-    // console.log('bang')
     getRandomLetter()
     inputLetter.value = ''
   }
 })
 
-// mounted() {
-// 	window.addEventListener('keypress', this._keyListener);
-// },
+
+onBeforeMount(() => {
+  getRandomLetter()
+});
 
 // beforeDestroy() {
 // 	window.removeEventListener('keypress', this._keyListener);
 // },
-
-onBeforeMount(() => {
-  getRandomLetter()
-  window.addEventListener('keydown', handleKeyDown);
-  // window.addEventListener('keypress', listenForKeyEvents);
-});
 
   // onBeforeMount(() => {
   //   console.log("Before Mount!");
