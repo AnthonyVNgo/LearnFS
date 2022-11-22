72afde2d431cef8699042fcc7c9edab55edfd1e1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-11-22 15:22:59 -0800

    feat: add clear input fn

--- src/views/FSTypeWriterView.vue
+++ src/views/FSTypeWriterView.vue
@@ -10,11 +10,11 @@
 
     <template v-slot:cards>
       <div class="d-flex flex-wrap justify-content-center">
-          <h1 v-if="!inputWordArray.length">Type + Enter below</h1>
-          <div v-else v-for="letter in inputWordArray" class="card ratio ratio-1x1" style="min-width: 80px; max-width: 10%;">
-            <img :src="`../../public/images/${letter}.png`" alt="" srcset="" style="object-fit: contain;" class="card-body">
-          </div>
+        <h1 v-if="!inputWordArray.length">Type + Enter below</h1>
+        <div v-else v-for="letter in inputWordArray" class="card ratio ratio-1x1" style="min-width: 80px; max-width: 10%;">
+          <img :src="`../../public/images/${letter}.png`" alt="" srcset="" style="object-fit: contain;" class="card-body">
         </div>
+      </div>
     </template>
 
     <template v-slot:input>
@@ -31,6 +31,12 @@
           maxlength="16" 
           placeholder="Type here + Enter" 
           autocomplete="off">
+          <button type="button" class="btn btn-dark" @click="clearInput">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+            </svg>
+          </button>
         </div>
       </form>
     </template>
@@ -63,5 +69,10 @@ function checkInput() {
 function generateInputWordArray() {
   const inputWordSplit = userInput.value.split('')
   inputWordArray.value = inputWordSplit
+}
+
+function clearInput() {
+  userInput.value = ''
+  generateInputWordArray()
 }
 </script>
